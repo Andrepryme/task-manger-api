b5a89ef7e048789c99195ba835437b43f581e37b
Andrew <andrepryme@gmail.com>
2026-01-11 03:37:13 +0100

    Fixed utils/logger.js to only log console information in non production environment

--- utils/logger.js
+++ utils/logger.js
@@ -1,9 +1,6 @@
-// Utility module for logging messages
-const { NODE_ENV } = require("../config/env");
-
 // Logs informational messages
 function logInfo(message) {
-    if (NODE_ENV !== "production") {
+    if (process.env.NODE_ENV !== "production") {
         console.log(`INFO: ${message}`);
     }
 }
